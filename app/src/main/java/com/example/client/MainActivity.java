258847c2decb172b61cdaf9b6aceda0347d591fe
katerina-chernetseva <k.chernetseva3108@gmail.com>
2020-04-14 23:18:36 +0300

    update project

--- app/src/main/java/com/example/client/MainActivity.java
+++ app/src/main/java/com/example/client/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnSignIn, btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn=findViewById(R.id.buttonSignIn);
+        btnRegister=findViewById(R.id.buttonRegister);
     }
 }
