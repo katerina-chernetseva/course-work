4a8e2b1de8c0fae065c9679c594cd169438712fc
katerina-chernetseva <k.chernetseva3108@gmail.com>
2020-04-20 19:08:49 +0300

    adding db connection

--- app/src/main/java/com/example/client/ConnectionHelper.java
+++ app/src/main/java/com/example/client/ConnectionHelper.java
@@ -1,5 +1,33 @@
 package com.example.client;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionHelper {
+    String IP, DB, DBUserName, DBPassword;
+    @SuppressLint("NewApi")
+    public Connection connection() throws ClassNotFoundException {
+        IP="";
+        DB="";
+        DBUserName="";
+        DBPassword="";
+
+        StrictMode.ThreadPolicy policy=new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        java.sql.Connection connection=null;
+        String ConnectionURL=null;
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL = "jdbc:jtds:sqlserver://" + IP + ";databaseName=" + DB + ";user=" + DBUserName + ";password=" + DBPassword + ";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 
 }
